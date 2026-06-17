68f11001a210955a4a34243880aeff28b548e10c
Manas Ranjan Bera <manasbera484@gmail.com>
2026-06-17 10:16:28 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,16 @@
+l = [1,2,3,True,False,"Hello",None]
+str1 = "Hello World"
+str2 = '12345678'
+str3= '''Hello World
+This is a multi-line string.
+It can span multiple lines.'''
+
+t1 = (1,2,3,True,False,"Hello",None)
+
+d1 = {"name": "Alice", 
+      "age": 30, 
+      "age": 25,
+      "city": "New York"}
+
+s1 = {1,1,1,1,2,3,4,5}
+print(s1)
