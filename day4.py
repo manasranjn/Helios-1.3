c7a9b002ebb61f3736896ba6b33c36625dd7e138
Manas Ranjan Bera <manasbera484@gmail.com>
2026-06-19 11:47:26 +0530

    notes on loops

---
+++ day4.py
@@ -0,0 +1,46 @@
+# Loop
+# for loop
+lst = [1, 2, 3, 4, 5]
+
+# for i in lst:
+#     print(i)
+
+# s = "Hello World"
+# for i in s:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,10, 2):
+#     print(i)
+
+# while loop
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# Write a program to print the first 10 natural numbers using a for loop and a while loop.
+
+# for i in range(1,11):
+#     print(i)
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# WAP to print the first 10 even numbers using a for loop and a while loop.
+
+
+count = 0
+for i in range(1,5):
+    for j in range(1,5):
+        print(i, j)
+        count += 1
+
+print(count)
